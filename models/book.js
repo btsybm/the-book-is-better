de3f25a343353eab06d6e719ee6cb3e90142939d
btsybm <betsybeam2@gmail.com>
2021-07-23 15:56:20 -0500

    book schema

--- models/book.js
+++ models/book.js
@@ -8,20 +8,20 @@ const Schema = mongoose.Schema
 
 
 const bookSchema = new Schema({
-  // title: {
-  //   type: String,
-  //   required: true
-  // },
-  // author: {
-  //   type: String,
-  //   required: true,
-  // },
-  // adaptations: [{type: Schema.Types.ObjectId, ref: 'Movie'}],
-  // imageUrl: {
-  //   type: String,
-  //   required: false,
-  // }, 
-  //   timestamps: true
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  adaptations: [{type: Schema.Types.ObjectId, ref: 'Movie'}],
+  imageUrl: {
+    type: String,
+    required: false,
+  }, 
+    timestamps: true
   })
 
 
