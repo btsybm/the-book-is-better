64474ccc3baecd13629dd4ff61261a5f2a11e655
btsybm <betsybeam2@gmail.com>
2021-07-24 16:47:07 -0500

    form for new add view

--- controllers/books.js
+++ controllers/books.js
@@ -1,4 +1,8 @@
 import { Book } from '../models/book.js'
+import { Author } from '../models/author.js'
+import { Movie } from '../models/movie.js'
+
+
 
 export {
   newBook as new
--- routes/books.js
+++ routes/books.js
@@ -10,3 +10,4 @@ export {
 router.get('/new', booksCtrl.new)
 
 
+
