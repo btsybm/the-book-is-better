05d402b19b4048b754798c1d2ae9782ed9c6954a
btsybm <betsybeam2@gmail.com>
2021-07-24 00:22:13 -0500

    correct movie model

--- models/movie.js
+++ models/movie.js
@@ -7,34 +7,31 @@ export {
 const Schema = mongoose.Schema
 
 const movieSchema = new Schema({
-  // title: {
-  //   type: String,
-  //   required: true
-  // },
-  // sourceMaterial: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'Book',
-  // },
-  // imageUrl: {
-  //   type: String,
-  // }, 
-  // betterThanBook: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'Review',
-  // },
-  // year: {
-  //   type: Date,
-  // },
-  // format: {
-  //   type: String,
-  //   enum: ['Movie', 'TV Series', 'Miniseries'],
-  //   required: true,
-  // },
-  // cast: {
-  //   type: String,
-  // }, 
-  //   timestamps: true
-  })
-
+  title: {
+    type: String,
+    required: true
+  },
+  sourceMaterial: [{type: Schema.Types.ObjectId, ref: 'Book'}],
+  imageUrl: {
+    type: String,
+  }, 
+  betterThanBook: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Review',
+  }],
+  year: {
+    type: Date,
+  },
+  format: {
+    type: String,
+    enum: ['Movie', 'TV Series', 'Miniseries'],
+    required: true,
+  },
+  cast: {
+    type: String,
+  }, 
+}, {
+  timestamps: true
+})
 
 const Movie = mongoose.model('Movie', movieSchema)
