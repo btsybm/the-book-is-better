4c3a4e62d8ac8680878a4841965a2427d44f0b9b
btsybm <betsybeam2@gmail.com>
2021-07-24 00:30:12 -0500

    correct profile model

--- models/profile.js
+++ models/profile.js
@@ -6,24 +6,14 @@ export {
 
 const Schema = mongoose.Schema
 
-
-
-const profileSchema = new mongoose.Schema({
-  // name: String,
-  // avatar: String,
-  // votes: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: Review,
-  // },
-  // friends: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: Profile 
-  // },
-  // comments: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: Review
-  // }, 
-  // timestamps: true
+const profileSchema = new Schema({
+  name: String,
+  avatar: String,
+  votes: [{type: Schema.Types.ObjectId, ref: 'Review'}],
+  friends: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
+  comments: [{type: Schema.Types.ObjectId, ref: 'Review'}],
+}, {
+  timestamps: true
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
